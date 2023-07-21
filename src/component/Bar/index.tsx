d0532d4316fa3a65d70e2e2688ce35944028bb74
Sina <sina.yektay1990@gmail.com>
2023-07-21 13:08:20 +0200

    search bar is in place now

--- src/component/Bar/index.tsx
+++ src/component/Bar/index.tsx
@@ -13,18 +13,20 @@ import { memo } from "react";
 import { useBar } from "./index.hooks";
 import { ShoppingCart } from "../ShoppingCart";
 import Link from "next/link";
+import { SearchBar } from "../SearchBar";
 
 type BarProps = {};
 
 export const Bar = memo(({}: BarProps) => {
   const {} = useBar();
   const theme = useTheme();
-  const smallScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const smallScreen = useMediaQuery(theme.breakpoints.down("lg"));
+  const mediumScreen = useMediaQuery(theme.breakpoints.down("md"));
 
   return (
     <AppBar position="static" sx={{ margin: 0, backgroundColor: "purple" }}>
       <Toolbar>
-        <Stack direction="row" spacing={10}>
+        <Stack direction="row" spacing={ smallScreen && mediumScreen ? 4 : smallScreen ? 5 : 10 }>
           <Stack>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Mercato{" "}
@@ -33,16 +35,18 @@ export const Bar = memo(({}: BarProps) => {
             </Typography>
           </Typography>
           </Stack>
-          <Stack sx={{justifyContent:"center"}}>
-          <Link href={"/About"}>
-            <Typography sx={{ color: "white" }}>Chi Siamo</Typography>
-          </Link>
-          </Stack>
-          <Stack sx={{justifyContent:"center"}}>
-          <Link href={"/offerte"}>
-            <Typography sx={{ color: "white" }}>Offerte</Typography>
-          </Link>
-          </Stack>
+          {!mediumScreen && 
+          <><Stack sx={{ justifyContent: "center" }}>
+
+              <Link href={"/About"}>
+                <Typography sx={{ color: "white" }}>Chi Siamo</Typography>
+              </Link>
+            </Stack><Stack sx={{ justifyContent: "center" }}>
+                <Link href={"/offerte"}>
+                  <Typography sx={{ color: "white" }}>Offerte</Typography>
+                </Link>
+              </Stack></>
+          }
           <Box>
             <ShoppingCart />
           </Box>
@@ -54,7 +58,9 @@ export const Bar = memo(({}: BarProps) => {
               سوپرمارکت ایرانی
             </Typography>
           )}
+         
         </Stack>
+        <Stack sx={{margin:"auto"}}><SearchBar /></Stack>
       </Toolbar>
     </AppBar>
   );
---
+++ src/component/SearchBar/index.hooks.tsx
@@ -0,0 +1,53 @@
+import { IItem } from "@/model/server/Item";
+import { actions, selectors } from "@/spas/productsSpa/redux-store/slices";
+import { useMemo, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+export const useSearchBar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+    setAnchorEl(e.target);
+  };
+  const products = useSelector(selectors.products);
+  const dispatch = useDispatch();
+  const filteredProducts = useMemo(() => {
+    return products.filter((product) => {
+      return product.productName
+        .toLowerCase()
+        .includes(searchQuery.toLowerCase());
+    });
+  }, [searchQuery, products]);
+
+  const open = Boolean(anchorEl) && filteredProducts.length > 0;
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleItemClick = (item: IItem) => {
+    setAnchorEl(null);
+    dispatch(
+      actions.changeDialogState({
+        productDialog: {
+          isDialogOpen: true,
+          productName: item.productName,
+          productId: item.productId,
+          price: item.price,
+          quantity: item.quantity,
+          image: item.image,
+        },
+      })
+    );
+  };
+
+  return {
+    searchQuery,
+    handleSearchChange,
+    filteredProducts,
+    open,
+    anchorEl,
+    handleClose,
+    handleItemClick,
+  };
+};
